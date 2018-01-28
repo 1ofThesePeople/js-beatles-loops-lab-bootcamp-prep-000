9475cc7f03113308536213b181c5664f51b86854
Jeremy Che <jeremy.che48@gmail.com>
2018-01-28 10:14:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ function theBeatlesPlay(musicians, instruments) {
   var temp = []
   for(var i = 0; i < musicians.length(); i++) {
     var string = musicians[i] + "plays" + instruments[i]
+    temp.push(string)
   }
+  return temp
 }
